b61ee9d17793616056ff9a3a87cceff4a5acf27a
olivierrivard <rivard.olivier@gmail.com>
2016-06-28 19:22:52 -0400

    Part 4 tutorial

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,7 @@
     import { Component } from '@angular/core';
 
-    export class Hero {
-        id: number;
-        name: string;
-    }
+    import { Hero } from './hero';
+    import { HeroDetailComponent } from './hero-detail.component';
 
     const HEROES: Hero[] = [
         { id: 11, name: 'Mr. Nice' },
@@ -30,15 +28,8 @@
             <span class="badge">{{hero.id}}</span> {{hero.name}}
          </li>
       </ul>
-      <div *ngIf="selectedHero">
-          <h2>{{selectedHero.name}} details!</h2>
-          <div><label>id: </label>{{selectedHero.id}}</div>
-          <div><label>name: </label>{{selectedHero.name}}</div>
-          <div>
-            <label>name: </label>
-            <input [(ngModel)]="selectedHero.name" placeholder="name">
-          </div>
-      </div>`,
+      <my-hero-detail [hero]="selectedHero"></my-hero-detail>
+      `,
         styles: [`
       .selected {
         background-color: #CFD8DC !important;
@@ -87,7 +78,8 @@
         margin-right: .8em;
         border-radius: 4px 0 0 4px;
       }
-    `]
+    `],
+        directives: [HeroDetailComponent]
 
     })
 
